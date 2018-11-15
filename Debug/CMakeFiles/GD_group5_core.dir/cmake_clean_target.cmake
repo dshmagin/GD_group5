e57ea50f2b9d8c8beb9f06519c8291d2dc609ace
dshmagin <dmytro.shmagin@gmail.com>
2018-11-15 08:58:48 -0500

    Deleted unnecessary files and draft work

--- Debug/CMakeFiles/GD_group5_core.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libGD_group5_core.a"
-)
--- include/TitleScreen.h
+++ include/TitleScreen.h
@@ -35,6 +35,34 @@ class TitleScreen
         bool musicCanPlay = true;
         float titleImage = 0;
 
+	sf::Text walk_left;
+	sf::Text walk_right;
+	sf::Text walk_down;
+	sf::Text walk_up;
+	sf::Text shoot_left;
+	sf::Text shoot_right;
+	sf::Text shoot_down;
+	sf::Text shoot_up;
+	sf::Text special_1;
+	sf::Text special_2;
+	sf::Text special_3;
+	sf::Text special_4;
+
+	sf::Keyboard::Key walk_left_key = sf::Keyboard::A;
+	sf::Keyboard::Key walk_right_key = sf::Keyboard::D;
+	sf::Keyboard::Key walk_down_key = sf::Keyboard::S;
+	sf::Keyboard::Key walk_up_key = sf::Keyboard::W;
+	sf::Keyboard::Key shoot_left_key = sf::Keyboard::Left;
+	sf::Keyboard::Key shoot_right_key = sf::Keyboard::Right;
+	sf::Keyboard::Key shoot_down_key = sf::Keyboard::Down;
+	sf::Keyboard::Key shoot_up_key = sf::Keyboard::Up;
+	sf::Keyboard::Key special_1_key = sf::Keyboard::Num1;
+	sf::Keyboard::Key special_2_key = sf::Keyboard::Num2;
+	sf::Keyboard::Key special_3_key = sf::Keyboard::Num3;
+	sf::Keyboard::Key special_4_key = sf::Keyboard::Num4;
+
+	bool keybind_chosen = true;
+	int keybindSelected = 0;
     public:
         TitleScreen();
         TitleScreen(shared_ptr<sf::RenderWindow> &window_ptr );
@@ -44,6 +72,12 @@ class TitleScreen
         int  getSelectedElement();
         void drawTitleScreen( float deltaTime );
         void drawElementOption( float deltaTime );
+        void drawOptionsScreen( float deltaTime );
+        int  getSelectedKeybind();
+	void setSelectedKeybind( int direction );
+        string keyToString(const sf::Keyboard::Key& key );
+        void newKeybind(const sf::Keyboard::Key& key );
+        void choosingKeybind();
         void startMusic();
         void stopMusic();
 };
