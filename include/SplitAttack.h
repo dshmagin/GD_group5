7c59390e130e774d98b32564c039d5f59ac70706
shortc <shortwchris@gmail.com>
2018-12-06 01:10:19 -0500

    Add element

--- include/SplitAttack.h
+++ include/SplitAttack.h
@@ -14,10 +14,11 @@ private:
 	float speed = 0.4;
 	float moveDistance;
 	int spriteNum = 0;
+	int element;
 
 public:
 	SplitAttack(){};
-	SplitAttack(shared_ptr<sf::RenderWindow> window_ptr, float rotation, Player* player_ptr);
+	SplitAttack(shared_ptr<sf::RenderWindow> window_ptr, float rotation, Player* player_ptr, int element);
     ~SplitAttack(){};
 
     void update(float deltaTime);
